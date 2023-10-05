f9be81b77041043d806dc45349428281de8259fa
DouglasSouza05 <douglasbsouza01@gmail.com>
2023-10-04 21:39:04 -0300

    Ambiente Maven

---
+++ exercicio/src/test/java/main/java/inatel/AppTest.java
@@ -0,0 +1,11 @@
+package main.java.inatel;
+
+// import static org.junit.Assert.assertTrue;
+// import org.junit.Test;
+
+// public class AppTest {
+// @Test
+// public void shouldAnswerWithTrue() {
+// assertTrue(true);
+// }
+// }
