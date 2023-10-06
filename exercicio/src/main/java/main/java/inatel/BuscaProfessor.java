a471b78e8b8bf954ef281c249348e819c29a6e0b
Douglas Brandão de Souza <douglasbsouza01@gmail.com>
2023-10-06 13:51:27 -0300

    Aplicando método ProfessorExists de BuscaService

--- exercicio/src/main/java/main/java/inatel/BuscaProfessor.java
+++ exercicio/src/main/java/main/java/inatel/BuscaProfessor.java
@@ -1,16 +1,26 @@
 package main.java.inatel;
 
-import java.util.ArrayList;
-
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+// import java.util.ArrayList;
+// import com.google.gson.JsonObject;
+// import com.google.gson.JsonParser;
 
 public class BuscaProfessor {
 
     BuscaService buscaService;
+    boolean professorExists;
 
-    public BuscaProfessor(BuscaService service) {
-        this.buscaService = service;
+    public BuscaProfessor(BuscaService buscaService) {
+        this.buscaService = buscaService;
+    }
+
+    public boolean ProfessorExists(int id) {
+        professorExists = buscaService.professorExists(id);
+
+        if (professorExists) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
