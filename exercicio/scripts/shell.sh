f2a541659e30ce00a35e5b38c7be0dbf3d89b257
Eduardo Resende <89862448+EduCR03@users.noreply.github.com>
2023-10-07 22:21:07 -0300

    Enviando um e mail confirmando para o Douglas

--- exercicio/scripts/shell.sh
+++ exercicio/scripts/shell.sh
@@ -1,3 +1,3 @@
 sudo apt-get install mailutils
 echo "Fim da instalacao"
-echo "Mandando um e mail para falar que nosso projeto tem integracao continua" | mail -s "a subject" ${EMAIL_LIST}
+echo "Mandando um e mail para falar que nosso projeto tem integracao continua" | mail -s "Pipeline do trabalho pratico de C214" ${EMAIL_LIST}
